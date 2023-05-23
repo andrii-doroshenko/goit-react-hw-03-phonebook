302650b075b08082a00c31f8f4d17649972b501c
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-05-23 13:38:23 +0300

    adding localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,22 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
   componentDidMount() {
-    console.log('componentDidMount');
+    const getSavedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(getSavedContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   getElId = () => nanoid(5);
@@ -50,9 +51,15 @@ class App extends Component {
   };
 
   handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+    const confirmDelete = window.confirm(
+      'Ви впевнені, що хочете видалити цей контакт?'
+    );
+
+    if (confirmDelete) {
+      this.setState(prevState => ({
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      }));
+    }
   };
 
   render() {
